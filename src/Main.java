5ea2aa2b9b51919fbac3e1c13e36da0237f29586
Yuup van Engelshoven <Yuup van Engelshoven>
2017-10-18 08:31:18 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
 	public static void main(String[] args) {
-		POMDP pomdp = ParsePOMDP.readPOMDP("domains/hallway2.POMDP");
+		//POMDP pomdp = ParsePOMDP.readPOMDP("domains/hallway2.POMDP");
 		//POMDP pomdp = ParsePOMDP.readPOMDP("domains/tiger.aaai.POMDP");
-		//POMDP pomdp = ParsePOMDP.readPOMDP("domains/saci-s100-a10-z31.POMDP");
+		POMDP pomdp = ParsePOMDP.readPOMDP("domains/saci-s100-a10-z31.POMDP");
 		VI vi = new VI(pomdp);
 		vi.Solve();
 		// pomdp.getTransitionProbability(s, a, sNext);
